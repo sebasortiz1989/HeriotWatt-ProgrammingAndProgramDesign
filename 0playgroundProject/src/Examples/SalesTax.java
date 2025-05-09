3b5f66b39e9faf73ec8bb56bb517e6d6c5c2ddd2
Sebastian <sebasortizsanta>
2025-05-08 22:15:29 -0300

    Sebas: [add] First program in java

---
+++ 0playgroundProject/src/Examples/SalesTax.java
@@ -0,0 +1,16 @@
+package Examples;
+
+import java.util.Scanner;
+
+public class SalesTax {
+    public static void main(String[] args) {
+        var input = new Scanner(System.in);
+
+        System.out.print("Enter purchase amount: ");
+        double purchaseAmount = input.nextDouble();
+
+        double tax = purchaseAmount * 0.06;
+
+        System.out.println("Sales Tax is $" + (int)(tax * 100) / 100.0);
+    }
+}
