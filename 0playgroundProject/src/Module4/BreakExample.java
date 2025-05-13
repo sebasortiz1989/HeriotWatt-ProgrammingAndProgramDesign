405784c2de8c8cfd87652da529c24b0022df1ec2
Sebastian <sebasortizsanta>
2025-05-12 21:51:56 -0300

    Sebas: [add] Loops

---
+++ 0playgroundProject/src/Module4/BreakExample.java
@@ -0,0 +1,14 @@
+package Module4;
+
+public class BreakExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5)
+                break;
+
+            System.out.println("i: " + i);
+        }
+
+        System.out.println("Loop complete.");
+    }
+}
---
+++ 0playgroundProject/src/Module4/ForLoopExample.java
@@ -0,0 +1,12 @@
+package Module4;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // Simple for loop to print numbers from 1 to 5
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Number: " + i);
+        }
+
+        System.out.println("Loop has ended.");
+    }
+}
