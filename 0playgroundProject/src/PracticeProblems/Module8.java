4eafcbf0609026dba91bd839b6dabefdc742c7b1
Sebastian <sebasortizsanta>
2025-07-16 21:25:58 -0300

    Sebas: [add] Office Hours 1

--- 0playgroundProject/src/PracticeProblems/Module8.java
+++ 0playgroundProject/src/PracticeProblems/Module8.java
@@ -1,35 +1,30 @@
-package Module9;
+package PracticeProblems;
 
-abstract class Animal {
-    abstract void makeSound();
-    void eat() {
-        System.out.println("This animal eats food.");
+class Animal {
+    public void makeSound() {
+        System.out.println("The animal makes sound");
     }
 }
 
 class Dog extends Animal {
-    @Override
-    void makeSound() {
+    public void makeSound() {
         System.out.println("Woof!");
     }
 }
 
 class Cat extends Animal {
     @Override
-    void makeSound() {
+    public void makeSound() {
         System.out.println("Meow!");
     }
 }
 
-public class AnimalSoundDemo {
+public class Module8 {
     public static void main(String[] args) {
         Animal myDog = new Dog();
         Animal myCat = new Cat();
 
         myDog.makeSound();  // Output: Woof!
-        myDog.eat();        // Output: This animal eats food.
-
         myCat.makeSound();  // Output: Meow!
-        myCat.eat();        // Output: This animal eats food.
     }
 }
